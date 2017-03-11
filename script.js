4c1930216af3a9e14dc8e250103fbe051b812759
Piotr Sito <jannowak@example.com>
2017-03-11 08:31:42 -0500

    add

---
+++ script.js
@@ -0,0 +1,7 @@
+var a=40;
+var b=20;
+var value=(a*a) + (2 * a * b) - (b*b);
+if (value>0) {console.log('wynik dodatni')}
+else if (value<0) {console.log('wynik ujemny')}
+else  {console.log('wynik zerowy')}
+
